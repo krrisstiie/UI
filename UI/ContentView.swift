1272acf8fc138f316796ee8932266f7242eded70
krrisstiie <kristiefongg@gmail.com>
2024-06-25 09:44:13 -0700

    Initial Commit

---
+++ UI/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  UI
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var name = ""
+    @State var textTitle = "What is your name?"
+    
+    var body: some View {
+       
+        VStack {
+            Text(textTitle)
+                .font(.title)
+                
+            TextField("Type your name here", text: $name)
+                .padding()
+                .multilineTextAlignment(.center)
+                .border(Color.black, width: 2)
+            
+            Button("Enter") {
+                textTitle = "Welcome, \(name)"
+                
+            } // close button
+            .font(.body)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
+        } // close VStack
+        
+        .padding()
+        
+    } // close body
+    
+} // close struct
+
+#Preview {
+    ContentView()
+}
---
+++ UI/UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIApp.swift
+//  UI
+//
+//  Created by Scholar on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
